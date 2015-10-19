0ebf772dfbc0c38995a33131bc06151514a44af6
Samuel Bowler <samuel.bowler@gmail.com>
2015-10-19 17:12:32 -0500

    Passing tests for init, app behavior: sending

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,25 @@
 // YOUR CODE HERE:
 
+var app = {};
+
+app.init = function(value)   {
+  return true;
+};
+
+app.send = function(message){
+  //should submit post request
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent. Data: ', data);
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message. Error: ', data);
+    }  
+  });
+};
+
