e0026b4c550487412abc04c65772bb0109aa5472
Samuel Bowler <samuel.bowler@gmail.com>
2015-10-19 17:25:01 -0500

    Passing tests for app.fetching

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -9,9 +9,24 @@ app.init = function(value)   {
 app.send = function(message){
   //should submit post request
   $.ajax({
-    url: 'https://api.parse.com/1/classes/chatterbox',
-    type: 'POST',
-    data: JSON.stringify(message),
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent. Data: ', data);
+      },
+      error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message. Error: ', data);
+    }  
+  });
+};
+
+app.fetch = function() {
+ $.ajax({
+    type: 'GET',
+    data: JSON.stringify(),
     contentType: 'application/json',
     success: function (data) {
       console.log('chatterbox: Message sent. Data: ', data);
@@ -22,4 +37,5 @@ app.send = function(message){
     }  
   });
 };
+
 
