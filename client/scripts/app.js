56d1a9a026c31a212f88393a05671d63979878ae
Samuel Bowler <samuel.bowler@gmail.com>
2015-10-19 22:00:05 -0500

    Fetching messages from Parse, displaying most recent messages on the screen

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -9,10 +9,14 @@ app.init = function(value)   {
   return true;
 };
 
+//Array objectIDs
+var objectIDs = [];
+
 
 //App send method
 app.send = function(message){
   //should submit post request
+  //Variable to hold ObjectID value;
   $.ajax({
       url: 'https://api.parse.com/1/classes/chatterbox',
       type: 'POST',
@@ -26,16 +30,23 @@ app.send = function(message){
       console.error('chatterbox: Failed to send message. Error: ', data);
     }  
   });
+  // objectIDs.push(data.objectId);
 };
 
 
 //App fetch method
 app.fetch = function() {
  $.ajax({
+    url: 'https://api.parse.com/1/classes/chatterbox?order=-createdAt',
     type: 'GET',
     data: JSON.stringify(),
     contentType: 'application/json',
-    success: function (data) {
+    success: function (data){ 
+      
+      //messages form server - messages is the array of message objects
+      var messages = data.results;
+      showMessage(messages);
+
       console.log('chatterbox: Message fetched. Data: ', data);
     },
     error: function (data) {
@@ -55,7 +66,12 @@ app.clearMessages = function() {
 //App add messages method
 app.addMessage = function(message){
   $('#chats').append('<div/>' + message + '</div>');
+  //Parse out and put in individual elements:
+    //username --> message.username
+    //text --> message.text
+    //roomname --> message.roomname
 };
+
 
 //App add room method
 app.addRoom = function(roomName){
@@ -63,6 +79,24 @@ app.addRoom = function(roomName){
 };
 
 
+//App add friend method
+app.addFriend = function() {
+
+};
 
 
+//App showMessage method
+var showMessage = function(messages) {
+  var reverseMessages = messages.reverse();
+  //iterate through array of message objects, put them in divs, and append to html
+  _.each(reverseMessages, function(messageObject) {
+    $('#chats').append('<div/>' + messageObject.text + '</div>')
+  });
+};
+
+app.fetch();
+
+
+//QUESTIONS OUTSTANDING
+//1. How do we access the most recent messages, if the Data.results array is capped at 100?
 
